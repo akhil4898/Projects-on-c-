4fda04cee3669124f84a10d99df4653926820612
akhil4898 <akhil.upadhyay_bca17@gla.ac.in>
2023-02-22 12:22:17 +0530

    Update repo

---
+++ c++/sumFromOneToN.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+int printSum(int n)
+{
+    int sum = 0;
+    for (int i = 1; i <= n; i++)
+    {
+        sum = sum + i;
+    }
+    return sum;
+}
+
+
+int main()
+{
+
+    int n;
+    cout << "Enter num: ";  
+    cin >> n;
+    cout << "Sum is: " << printSum(n) << endl;
+    return 0;
+}
