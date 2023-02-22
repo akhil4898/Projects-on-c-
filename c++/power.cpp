de553dcb9b3d9cdecb0eea3c498e943a92621f6c
akhil4898 <akhil.upadhyay_bca17@gla.ac.in>
2023-02-22 16:08:37 +0530

    Update file

--- c++/power.cpp
+++ c++/power.cpp
@@ -2,21 +2,35 @@
 
 using namespace std;
 
-int power(int n)
+int power(int exponent)
 {
     int result = 2;
-    for (int i = 1; i < n; i++)
+    for (int i = 1; i < exponent; i++)
     {
         result = result * 2;
     }
     return result;
 }
 
+
+int power(int base, int exponent)
+{
+    int power = base;
+    for (int i = 1; i < exponent; i++)
+    {
+            power = power * base;   
+    }
+    return power;
+}
+
 int main()
 {
-    int n;
-    cout << "Enter num: ";
-    cin >> n;
-    cout << "Result is: " << power(n) << endl;
+    int base, exponent;
+    cout << "Enter base: ";
+    cin >> base;
+    cout << "Enter Exponent: ";
+    cin >> exponent;
+    cout << "Result is: " << power(exponent) << endl;
+    cout << "Result is: " << power(base, exponent) << endl;
     return 0;
 }
